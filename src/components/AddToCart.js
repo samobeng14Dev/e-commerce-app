9a71adf8f4aafabbdf31297da4571b45abc893c9
Samuel Obeng <obengsamuel270@gmail.com>
2024-04-29 20:12:52 +0530

    initial setup for addToCart functionality

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
+	const { addToCart } = useCartContext();
 	const { id, stock, colors } = product;
 	const [mainColor, setMainColor] = useState(colors[0]);
 	const [amount, setAmount] = useState(1);
@@ -55,7 +56,8 @@ const AddToCart = ({ product }) => {
 				/>
 				<Link
 					to='/cart'
-					className='btn'>
+					className='btn'
+					onClick={() => addToCart(id, mainColor, amount, product)}>
 					add to cart
 				</Link>
 			</div>
