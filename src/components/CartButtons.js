5694d51d4d7dabf6ec0fb69eeec9f6f20a08d732
Samuel Obeng <obengsamuel270@gmail.com>
2024-05-15 21:23:40 +0530

    Auth0 toggle logivn and logout values

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -22,17 +22,22 @@ const CartButtons = () => {
 					<span className='cart-value'>{total_items}</span>
 				</span>
 			</Link>
-			<button
-				type='button '
-				className='auth-btn'>
-				Login <FaUserPlus />
-			</button>
-			<button
-				type='submit'
-				className='auth-btn'>
-				Logout
-				<FaUserMinus />
-			</button>
+			{myUser ? (
+				<button
+					type='submit'
+					className='auth-btn'
+					onClick={() => logout({ returnTo: window.location.origin })}>
+					Logout
+					<FaUserMinus />
+				</button>
+			) : (
+				<button
+					type='button '
+					className='auth-btn'
+					onClick={loginWithRedirect}>
+					Login <FaUserPlus />
+				</button>
+			)}
 		</Wrapper>
 	);
 };
--- src/context/user_context.js
+++ src/context/user_context.js
@@ -7,12 +7,17 @@ export const UserProvider = ({ children }) => {
 		useAuth0();
 	const [myUser, setMyUser] = useState(null);
 	useEffect(() => {
-		console.log(`user:${user}`);
-		console.log(`isAuthenticated:${isAuthenticated}`);
-		console.log(`isLoading:${isLoading}`);
+		// console.log(`user:${user}`);
+		// console.log(`isAuthenticated:${isAuthenticated}`);
+		// console.log(`isLoading:${isLoading}`);
+		if (isAuthenticated) {
+			setMyUser(user);
+		} else {
+			setMyUser(false);
+		}
 	}, [isAuthenticated]);
 	return (
-		<UserContext.Provider value={{ loginWithRedirect, logout }}>
+		<UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
 			{children}
 		</UserContext.Provider>
 	);
