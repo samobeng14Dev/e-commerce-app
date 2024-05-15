a0a0a002b15233521ee48144d0225c986bb9dbc7
Samuel Obeng <obengsamuel270@gmail.com>
2024-05-15 20:45:03 +0530

    user context setup

--- src/index.js
+++ src/index.js
@@ -23,13 +23,15 @@ root.render(
 		authorizationParams={{
 			redirect_uri: window.location.origin,
 		}}>
-		<ProductsProvider>
-			<FilterProvider>
-				<CartProvider>
-					<App />
-				</CartProvider>
-			</FilterProvider>
-			,
-		</ProductsProvider>
+		<UserProvider>
+			<ProductsProvider>
+				<FilterProvider>
+					<CartProvider>
+						<App />
+					</CartProvider>
+				</FilterProvider>
+				,
+			</ProductsProvider>
+		</UserProvider>
 	</Auth0Provider>
 );
