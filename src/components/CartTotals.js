dc4edade002179b0c0f85f8c0a51bf5c43c42fd3
Samuel Obeng <obengsamuel270@gmail.com>
2024-05-15 21:47:54 +0530

    hide or  show checkout in navbar and sidebar

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 
 const CartTotals = () => {
 	const { total_amount, shipping_fees } = useCartContext();
+	const { myUser, loginWithRedirect } = useUserContext();
 	return (
 		<Wrapper>
 			<div>
@@ -22,11 +23,20 @@ const CartTotals = () => {
 						order total:<span>{formatPrice(total_amount + shipping_fees)}</span>
 					</h4>
 				</article>
-				<Link
-					to='/checkout'
-					className='btn'>
-					proceed to checkout
-				</Link>
+				{myUser ? (
+					<Link
+						to='/checkout'
+						className='btn'>
+						proceed to checkout
+					</Link>
+				) : (
+					<button
+						className='btn'
+						type='button'
+						onClick={loginWithRedirect}>
+						login
+					</button>
+				)}
 			</div>
 		</Wrapper>
 	);
