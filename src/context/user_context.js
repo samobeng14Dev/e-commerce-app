63e7c4d41b65ec07c1afc2b49eb4b3f1d0c54cc2
Samuel Obeng <obengsamuel270@gmail.com>
2024-05-24 05:49:05 -0700

    netlify cli installation

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,19 +3,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
-	const { loginWithRedirect, logout, user } = useAuth0();
+	const { loginWithRedirect, logout, user, isLoading, error } = useAuth0();
 	const [myUser, setMyUser] = useState(null);
+
 	useEffect(() => {
 		setMyUser(user);
-
-		// if (isAuthenticated) {
-		// 	setMyUser(user);
-		// } else {
-		// 	setMyUser(false);
-		// }
 	}, [user]);
+
 	return (
-		<UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
+		<UserContext.Provider
+			value={{ loginWithRedirect, logout, myUser, isLoading, error }}>
 			{children}
 		</UserContext.Provider>
 	);
